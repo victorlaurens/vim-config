03de99c7364f5bd6f1552bd358473709cf82ab24
autobuild <victorlaurens@gmail.com>
2018-08-08 15:19:03 +1000

    Add initial scripts to install config

---
+++ README.md
@@ -0,0 +1,4 @@
+# Installation Steps
+1. Create .vim folder
+2. Clone repo to .vim
+3. Run setup.sh
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install plugin
+vim +PluginInstall +qall
+
